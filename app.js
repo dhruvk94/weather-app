6cddae1a1581efbd6ff0c4f5170044c760142018
Dhruv Khatri <dhruv94k@gmail.com>
2017-07-12 01:38:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const request = require('request');
+const yargs = require('yargs');
+
+//encodeURIComponent('');
+//decodeURIComponent('');
+
+const argv = yargs
+	.options({
+		a: {
+			demand : true,
+			alias : 'address',
+			describle : 'Address to fetch weather for',
+			string : true
+		}
+	})
+	.help()
+	.alias('help','h')
+	.argv;
+	
+//console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 //Google geolocation apis
 request({
-url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+url : `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json : true
 },(error, response, body) => {
-	console.log(JSON.stringify(body, undefined, 2));
-//	console.log(response);
+//	console.log(JSON.stringify(body, undefined, 2));
+	if(error){
+		console.log('Unable to connect to Google servers.');
+	}
+	else if(body.status === 'ZERO_RESULTS'){
+		console.log('Unable to find address.');
+	}
+	else if(body.status === 'OK'){
+		console.log(`Formatted Address : ${body.results[0].formatted_address}`);
+		console.log(`Latitude : `+body.results[0].geometry.location.lat);
+		console.log(`Longitude : `+body.results[0].geometry.location.lng);
+	}
+	
+	//	console.log(response);
 });
