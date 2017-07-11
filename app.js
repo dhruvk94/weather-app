486b217ed7854354585c255f104008155a2862a2
Dhruv Khatri <dhruv94k@gmail.com>
2017-07-12 02:43:03 +0530

    Callback abstraction

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
-
+var geocode = require('./geocode/geocode');
 //encodeURIComponent('');
 //decodeURIComponent('');
 
@@ -16,26 +16,12 @@ const argv = yargs
 	.help()
 	.alias('help','h')
 	.argv;
+geocode.geocodeAddress(argv.address,(errorMsg, response) => {
+	if(errorMsg){
+		console.log(errorMsg);
+	}
+	else{
+		console.log(JSON.stringify(response,undefined,2));
+	}
+});	
 	
-//console.log(argv);
-var encodedAddress = encodeURIComponent(argv.address);
-//Google geolocation apis
-request({
-url : `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-	json : true
-},(error, response, body) => {
-//	console.log(JSON.stringify(body, undefined, 2));
-	if(error){
-		console.log('Unable to connect to Google servers.');
-	}
-	else if(body.status === 'ZERO_RESULTS'){
-		console.log('Unable to find address.');
-	}
-	else if(body.status === 'OK'){
-		console.log(`Formatted Address : ${body.results[0].formatted_address}`);
-		console.log(`Latitude : `+body.results[0].geometry.location.lat);
-		console.log(`Longitude : `+body.results[0].geometry.location.lng);
-	}
-	
-	//	console.log(response);
-});
